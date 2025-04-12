e9dd3c63f3c15012e7b311f119ad8fec2cbbc013
amalight <amalaitagames@protonmail.com>
2025-04-12 17:46:45 +0200

    - Ajout d'un uploader de fichier; - Ajout d'une méthode pour écrire à la suite d'un fichier déjà existant; - Réorganisation de l'aspect visuel de l'interface gradio.

--- audioToText.py
+++ audioToText.py
@@ -1,7 +1,6 @@
-import os
 import subprocess
 import whisper
-import speech_recognition as sr
+# import speech_recognition as sr
 
 # r = sr.Recognizer()
 # microphone = sr.Microphone()
@@ -13,14 +12,10 @@ def writeText(source) :
     audio = whisper.load_audio(source)
     audio = whisper.pad_or_trim(audio)
 
-    # make log-Mel spectrogram and move to the same device as the model
     mel = whisper.log_mel_spectrogram(audio, n_mels=model.dims.n_mels).to(model.device)
 
-    # detect the spoken language
     _, probs = model.detect_language(mel)
-    print(f"Detected language: {max(probs, key=probs.get)}")
 
-    # decode the audio
     options = whisper.DecodingOptions()
     result = whisper.decode(model, mel, options)
 
@@ -29,5 +24,4 @@ def writeText(source) :
 def open_file_in_editor(cmd_texte):
     file_name = cmd_texte.replace('Ouvre le fichier, ', '')
     file_path = f"downloads/text_exports/{file_name}".strip()
-    print(file_path)
     subprocess.call(["gnome-text-editor", file_path])
--- gradio_interface.py
+++ gradio_interface.py
@@ -24,41 +24,58 @@ def sauver_fichier(text_box, name_box):
             f.close()
         return file_path
 
-def clear_inputs(text_box, name_box, file_box, time_box):
+def clear_inputs(text_box, name_box, file_box, file_upldr, time_box):
     text_box.clear()
     name_box.clear()
     file_box.clear()
+    file_upldr.clear()
     time_box.clear()
 
 def downlaod_file(f_explore):
     return f_explore
 
+def afficher_nom_fichier(fichier):
+    return fichier
+
+def ecrire_dans_fichier_existant(file, text_to_append):
+    with open(file, 'a') as f:
+        f.write('\n' + text_to_append)
+        f.close()
+        return file
+
 callback = gr.CSVLogger()
 demo = gr.Blocks()
 
 with demo:
-    with gr.Group():
-        audio_file = gr.Audio(sources=["microphone", "upload"],type="filepath")
-        with gr.Row():
-            gr.Row(scale=2)
-            rec_button = gr.Button("Convert Voice to Text", size='lg', min_width=80, variant='huggingface')
-            gr.Row(scale=2)
-    textBox = gr.Textbox(label="Texte trouvé", scale=3)
-    timeBox = gr.Textbox(label="Time", scale=3)
-    nameBox = gr.Textbox(label="Nom du fichier", scale=3)
-    file_downloader = gr.File(label="Téléchargement")
     with gr.Row():
-        buttonSubmit = gr.Button("Créer fichier txt", size='lg')
-        button_flag = gr.Button("Flag les datas", size='lg')
-        clear_button = gr.ClearButton(components=[audio_file, textBox, nameBox, file_downloader, timeBox], size='lg')
-    with gr.Row():
-        timer = gr.Timer(1)
-        file_explorer = gr.FileExplorer(root_dir="./downloads", file_count="single", every=timer)
-        download_button = gr.DownloadButton("Télécharger un fichier", value=file_explorer.value)
+        with gr.Column():
+            with gr.Group():
+                audio_file = gr.Audio(sources=["microphone", "upload"],type="filepath")
+                with gr.Row():
+                    gr.Row()
+                    rec_button = gr.Button("Convertir voix en texte", size='lg', min_width=80, variant='huggingface')
+                    gr.Row()
+            textBox = gr.Textbox(label="Texte trouvé", scale=3)
+            timeBox = gr.Textbox(label="Temps de conversion", scale=3)
+            nameBox = gr.Textbox(label="Nom du fichier", scale=3)
+            file_downloader = gr.File(label="Téléchargement")
+        with gr.Column():
+            with gr.Row():
+                file_uploader = gr.File(label="Upload")
+            with gr.Row():
+                buttonSubmit = gr.Button("Créer fichier txt", size='lg')
+                buttonWrite = gr.Button("Écrire dans le fichier existant", size="lg")
+                button_flag = gr.Button("Flag les datas", size='lg')
+            clear_button = gr.ClearButton(value="Nouveau", variant="primary", components=[audio_file, textBox, nameBox, file_downloader, file_uploader, timeBox], size='lg')
+            with gr.Column():
+                timer = gr.Timer(1)
+                file_explorer = gr.FileExplorer(root_dir="./downloads", file_count="single", every=timer)
+                download_button = gr.DownloadButton("Télécharger un fichier", value=file_explorer.value)
 
     callback.setup([audio_file, textBox], "flagged_audio")
 
     buttonSubmit.click(fn=sauver_fichier, inputs=[textBox, nameBox], outputs=[file_downloader])
+    buttonWrite.click(fn=ecrire_dans_fichier_existant, inputs=[file_uploader, textBox], outputs=file_downloader)
     button_flag.click(lambda *args: callback.flag(list(args)), [audio_file, textBox], None, preprocess=False)
     rec_button.click(audio_to_text, inputs=audio_file, outputs=[textBox, timeBox])
     download_button.click(fn=downlaod_file, inputs=file_explorer, outputs=download_button)
